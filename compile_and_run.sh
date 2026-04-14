6c39e9c72e4578e2a347105320d235add73a3801
toha129 <ifthekharhamim@gmail.com>
2026-04-15 02:12:28 +0600

    Clean up repo: remove binaries, generated files, and temp files

--- compile_and_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /f/bongoscript_project
-gcc output.c -o runme.exe 2>&1
-if [ $? -ne 0 ]; then
-    echo "COMPILE_ERROR"
-    exit 1
-fi
-./runme.exe < user_input.txt 2>&1
--- output.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-setvbuf(stdout, NULL, _IONBF, 0);
-int x;
-scanf("%d", &x);
-printf("%d\n", x);
-printf("%s\n", "Program shesh.");
-return 0;
-}
